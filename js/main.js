577a4fe007bec5d31060c342a99a3841c0d29b57
Sulvrix_ <sultanbamarhool@gmail.com>
2024-05-12 22:39:21 +0300

    s

--- js/main.js
+++ js/main.js
@@ -371,173 +371,389 @@
   };
 }
 
-// {
-//   function RSA(p, q, msg, e) {
-//     // const bigint = require("big-integer");
-//     function gcd(a, h) {
-//       /*
-//        * This function returns the gcd or greatest common
-//        * divisor
-//        */
-//       let temp;
-//       while (true) {
-//         temp = a % h;
-//         if (temp === 0) return h;
-//         a = h;
-//         h = temp;
-//       }
-//     }
-
-//     function modInverse(e, phi) {
-//       let a = phi;
-//       let b = e;
-//       let x = 0;
-//       let y = 1;
-
-//       while (b > 0) {
-//         let q = Math.floor(a / b);
-//         let r = a % b;
-//         let m = x - q * y;
-//         a = b;
-//         b = r;
-//         x = y;
-//         y = m;
-//       }
-
-//       if (a !== 1) return null;
-
-//       return ((x % phi) + phi) % phi;
-//     }
-
-//     function modPow(base, exponent, modulus) {
-//       let result = 1;
-//       base = base % modulus;
-
-//       while (exponent > 0) {
-//         if (exponent & (1 === 1)) {
-//           result = (result * base) % modulus;
-//         }
-//         base = (base * base) % modulus;
-//         exponent = exponent >> 1;
-//       }
-
-//       return result;
-//     }
-
-//     // let p = document.getElementById("rsa-p").value;
-//     // let q = document.getElementById("rsa-q").value;
-
-//     // Stores the first part of public key:
-//     let n = p * q;
-
-//     // Finding the other part of public key.
-//     // let e stands for encrypt
-//     // let e = document.getElementById("rsa-e").value;
-//     let phi = (p - 1) * (q - 1);
-//     while (e < phi) {
-//       /*
-//        * e must be co-prime to phi and
-//        * smaller than phi.
-//        */
-//       if (gcd(e, phi) === 1) break;
-//       else e++;
-//     }
-//     // let k = 2; // A constant value
-//     // let d = (1 + k * phi) / e;
-//     let d = modInverse(e, phi); // Compute modular inverse
-
-//     // Message to be encrypted
-//     // let msg = document.getElementById("rsa-m").value;
-
-//     console.log("Message data = " + msg);
-
-//     // Encryption c = (msg ^ e) % n
-//     // let c = Math.pow(msg, e) % n;
-//     let c = modPow(msg, e, n);
-//     console.log("Encrypted data = " + c);
-
-//     // let m = (c ^ d) % n;
-//     // let m = Math.pow(c, d) % n;
-//     let m = modPow(c, d, n);
-//     console.log("Original Message Sent = " + m);
-
-//     return (result = [c, m]);
-//   }
-//   /////////////
-//   document.getElementById("rsa-encrypt").onclick = function () {
-//     let p = document.getElementById("rsa-p").value;
-//     let q = document.getElementById("rsa-q").value;
-//     let e = document.getElementById("rsa-e").value;
-//     let m = document.getElementById("rsa-m").value;
-//     let _result = RSA(p, q, m, e);
-//     document.getElementById("rsa-result").value = _result[0];
-//   };
-//   document.getElementById("rsa-decrypt").onclick = function () {
-//     let p = document.getElementById("rsa-p").value;
-//     let q = document.getElementById("rsa-q").value;
-//     let e = document.getElementById("rsa-e").value;
-//     let m = document.getElementById("rsa-m").value;
-//     let _result = RSA(p, q, m, e);
-//     document.getElementById("rsa-result").value = _result[1];
-//   };
-// }
 {
-  // rsaEncrypt() {
-  //   let p = document.getElementById("rsa-p").value;
-  //   let q = document.getElementById("rsa-q").value;
-  //   let e = document.getElementById("rsa-e").value;
-  //   let m = document.getElementById("rsa-m").value;
-  //   generateKeys(p, q, e);
-  //   cipherText = encrypt(m, publicKey);
-  // }
-  // rsaDecrypt() {
-  //   let p = document.getElementById("rsa-p").value;
-  //   let q = document.getElementById("rsa-q").value;
-  //   let e = document.getElementById("rsa-e").value;
-  //   let m = document.getElementById("rsa-m").value;
-  //   RSA.generateKeys(p, q, e);
-  //   cipherText = encrypt(m, privateKey);
-  // }
-}
-document.getElementById("rsa-encrypt").onclick = function () {
-  function modularExponentiation(base, exponent, modulus) {
-    let result = 1;
-    base = base % modulus;
-    while (exponent > 0) {
-      if (exponent % 2 === 1)
-        // If exponent is odd, multiply base with result
-        result = (result * base) % modulus;
-      exponent = Math.floor(exponent / 2); // Divide exponent by 2
-      base = (base * base) % modulus;
+  function RSA(p, q, msg, e) {
+    function gcd(a, h) {
+      /*
+       * This function returns the gcd or greatest common
+       * divisor
+       */
+      let temp;
+      while (true) {
+        temp = a % h;
+        if (temp === 0) return h;
+        a = h;
+        h = temp;
+      }
     }
-    return result;
-  }
 
-  function rsaEncrypt(p, q, e, plaintext) {
-    const N = p * q;
-    const plaintextNumbers = [...plaintext].map((char) => char.charCodeAt(0)); // Convert plaintext to array of ASCII values
-    const encrypted = plaintextNumbers.map((num) => {
-      if (num >= N) throw new Error("Plaintext character ASCII value cannot be greater than modulus N.");
-      return modularExponentiation(num, e, N); // Encrypt each number
-    });
-    return encrypted;
-  }
+    function modInverse(e, phi) {
+      let a = phi;
+      let b = e;
+      let x = 0;
+      let y = 1;
 
-  // Example usage:
-  const p = parseInt(prompt("Enter a small prime number p:"));
-  const q = parseInt(prompt("Enter a small prime number q:"));
-  const e = parseInt(prompt("Enter public exponent e:"));
-  const message = prompt("Enter a plaintext message:");
+      while (b > 0) {
+        let q = Math.floor(a / b);
+        let r = a % b;
+        let m = x - q * y;
+        a = b;
+        b = r;
+        x = y;
+        y = m;
+      }
 
-  try {
-    const encrypted = rsaEncrypt(p, q, e, message);
-    console.log("Encrypted message:", encrypted.join(" "));
-  } catch (error) {
-    console.error(error);
+      if (a !== 1) return null;
+
+      return ((x % phi) + phi) % phi;
+    }
+
+    function modPow(base, exponent, modulus) {
+      let result = 1;
+      base = base % modulus;
+
+      while (exponent > 0) {
+        if (exponent & (1 === 1)) {
+          result = (result * base) % modulus;
+        }
+        base = (base * base) % modulus;
+        exponent = exponent >> 1;
+      }
+
+      return result;
+    }
+
+    // Stores the first part of public key:
+    let n = p * q;
+
+    // Finding the other part of public key.
+    // let e stands for encrypt
+    let phi = (p - 1) * (q - 1);
+    while (e < phi) {
+      /*
+       * e must be co-prime to phi and
+       * smaller than phi.
+       */
+      if (gcd(e, phi) === 1) break;
+      else e++;
+    }
+    let d = modInverse(e, phi); // Compute modular inverse
+
+    let c = modPow(msg, e, n);
+
+    let m = modPow(c, d, n);
+
+    return (result = [c, m, msg]);
   }
-};
-// document.getElementById("rsa-decrypt").onclick = function () {
-//   // RSA = new RSA();
-//   let _result = RSA.rsaDecrypt();
-//   document.getElementById("rsa-result").value = _result[1];
-// };
+  /////////////
+  document.getElementById("rsa-encrypt").onclick = function () {
+    p = document.getElementById("rsa-p").value;
+    q = document.getElementById("rsa-q").value;
+    e = document.getElementById("rsa-e").value;
+    m = document.getElementById("rsa-m").value;
+    if (p === "" || q === "" || e === "" || m === "") {
+      document.getElementById("rsa-result").value = "";
+    } else {
+      let _result = RSA(p, q, m, e);
+      document.getElementById("rsa-result").value = _result[0];
+    }
+  };
+  document.getElementById("rsa-decrypt").onclick = function () {
+    let p = document.getElementById("rsa-p").value;
+    let q = document.getElementById("rsa-q").value;
+    let e = document.getElementById("rsa-e").value;
+    let c = document.getElementById("rsa-m").value; // Use 'c' for ciphertext input
+
+    if (p === "" || q === "" || e === "" || c === "") {
+      document.getElementById("rsa-result").value = "";
+    } else {
+      let _result = RSA(p, q, c, e); // Pass 'c' as the message for decryption
+      document.getElementById("rsa-result").value = _result[1]; // Display the decrypted message
+    }
+  };
+}
+
+{
+  function des(key, plaintext) {
+    // int key[]= {0,0,1,0,0,1,0,1,1,1};
+    // const key = [1, 0, 1, 0, 0, 0, 0, 0, 1, 0]; // extra example for checking purpose
+    const P10 = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6];
+    const P8 = [6, 3, 7, 4, 8, 5, 10, 9];
+
+    const key1 = new Array(8).fill(0);
+    const key2 = new Array(8).fill(0);
+
+    const IP = [2, 6, 3, 1, 4, 8, 5, 7];
+    const EP = [4, 1, 2, 3, 2, 3, 4, 1];
+    const P4 = [2, 4, 3, 1];
+    const IP_inv = [4, 1, 3, 5, 7, 2, 8, 6];
+
+    const S0 = [
+      [1, 0, 3, 2],
+      [3, 2, 1, 0],
+      [0, 2, 1, 3],
+      [3, 1, 3, 2],
+    ];
+    const S1 = [
+      [0, 1, 2, 3],
+      [2, 0, 1, 3],
+      [3, 0, 1, 0],
+      [2, 1, 0, 3],
+    ];
+
+    // this function basically generates the key(key1 and key2) using P10 and P8 with (1 and 2)left shifts
+    const key_generation = () => {
+      const key_ = new Array(10);
+
+      for (let i = 0; i < 10; i++) {
+        key_[i] = key[P10[i] - 1];
+      }
+
+      const Ls = new Array(5);
+      const Rs = new Array(5);
+
+      for (let i = 0; i < 5; i++) {
+        Ls[i] = key_[i];
+        Rs[i] = key_[i + 5];
+      }
+
+      const Ls_1 = shift(Ls, 1);
+      const Rs_1 = shift(Rs, 1);
+
+      for (let i = 0; i < 5; i++) {
+        key_[i] = Ls_1[i];
+        key_[i + 5] = Rs_1[i];
+      }
+
+      for (let i = 0; i < 8; i++) {
+        key1[i] = key_[P8[i] - 1];
+      }
+
+      const Ls_2 = shift(Ls, 2);
+      const Rs_2 = shift(Rs, 2);
+
+      for (let i = 0; i < 5; i++) {
+        key_[i] = Ls_2[i];
+        key_[i + 5] = Rs_2[i];
+      }
+
+      for (let i = 0; i < 8; i++) {
+        key2[i] = key_[P8[i] - 1];
+      }
+
+      console.log("Your Key-1 :");
+      console.log(key1.join(" "));
+      console.log("Your Key-2 :");
+      console.log(key2.join(" "));
+    };
+
+    // this function is use full for shifting(circular) the array n position towards left
+    const shift = (ar, n) => {
+      while (n > 0) {
+        const temp = ar[0];
+        for (let i = 0; i < ar.length - 1; i++) {
+          ar[i] = ar[i + 1];
+        }
+        ar[ar.length - 1] = temp;
+        n--;
+      }
+      return ar;
+    };
+
+    // this is main encryption function takes plain text as input uses another functions and returns the array of cipher text
+    const encryption = (plaintext) => {
+      const arr = new Array(8);
+
+      for (let i = 0; i < 8; i++) {
+        arr[i] = plaintext[IP[i] - 1];
+      }
+      const arr1 = function_(arr, key1);
+
+      const after_swap = swap(arr1, arr1.length / 2);
+
+      const arr2 = function_(after_swap, key2);
+
+      const ciphertext = new Array(8);
+
+      for (let i = 0; i < 8; i++) {
+        ciphertext[i] = arr2[IP_inv[i] - 1];
+      }
+
+      return ciphertext;
+    };
+
+    // decimal to binary string 0-3
+    const binary_ = (val) => {
+      if (val === 0) return "00";
+      else if (val === 1) return "01";
+      else if (val === 2) return "10";
+      else return "11";
+    };
+
+    // this function is doing core things like expansion then xor with desired key then S0 and S1 substitution P4 permutation and again xor we have used this function 2 times(key-1 and key-2) during encryption and 2 times(key-2 and key-1) during decryption
+    const function_ = (ar, key_) => {
+      const l = new Array(4);
+      const r = new Array(4);
+
+      for (let i = 0; i < 4; i++) {
+        l[i] = ar[i];
+        r[i] = ar[i + 4];
+      }
+
+      const ep = new Array(8);
+
+      for (let i = 0; i < 8; i++) {
+        ep[i] = r[EP[i] - 1];
+      }
+
+      for (let i = 0; i < 8; i++) {
+        ar[i] = key_[i] ^ ep[i];
+      }
+
+      const l_1 = new Array(4);
+      const r_1 = new Array(4);
+
+      for (let i = 0; i < 4; i++) {
+        l_1[i] = ar[i];
+        r_1[i] = ar[i + 4];
+      }
+
+      let row, col, val;
+
+      row = parseInt(`${l_1[0]}${l_1[3]}`, 2);
+      col = parseInt(`${l_1[1]}${l_1[2]}`, 2);
+      val = S0[row][col];
+      const str_l = binary_(val);
+
+      row = parseInt(`${r_1[0]}${r_1[3]}`, 2);
+      col = parseInt(`${r_1[1]}${r_1[2]}`, 2);
+      val = S1[row][col];
+      const str_r = binary_(val);
+
+      const r_ = new Array(4);
+      for (let i = 0; i < 2; i++) {
+        const c1 = str_l.charAt(i);
+        const c2 = str_r.charAt(i);
+        r_[i] = parseInt(c1, 10);
+        r_[i + 2] = parseInt(c2, 10);
+      }
+      const r_p4 = new Array(4);
+      for (let i = 0; i < 4; i++) {
+        r_p4[i] = r_[P4[i] - 1];
+      }
+
+      for (let i = 0; i < 4; i++) {
+        l[i] = l[i] ^ r_p4[i];
+      }
+
+      const output = new Array(8);
+      for (let i = 0; i < 4; i++) {
+        output[i] = l[i];
+        output[i + 4] = r[i];
+      }
+      return output;
+    };
+
+    // this function swaps the nibble of size n(4)
+    const swap = (array, n) => {
+      const l = new Array(n);
+      const r = new Array(n);
+
+      for (let i = 0; i < n; i++) {
+        l[i] = array[i];
+        r[i] = array[i + n];
+      }
+
+      const output = new Array(2 * n);
+      for (let i = 0; i < n; i++) {
+        output[i] = r[i];
+        output[i + n] = l[i];
+      }
+
+      return output;
+    };
+
+    // this is main decryption function
+    // here we have used all previously defined function
+    // it takes cipher text as input and returns the array of decrypted text
+    const decryption = (ar) => {
+      const arr = new Array(8);
+
+      for (let i = 0; i < 8; i++) {
+        arr[i] = ar[IP[i] - 1];
+      }
+
+      const arr1 = function_(arr, key2);
+
+      const after_swap = swap(arr1, arr1.length / 2);
+
+      const arr2 = function_(after_swap, key1);
+
+      const decrypted = new Array(8);
+
+      for (let i = 0; i < 8; i++) {
+        decrypted[i] = arr2[IP_inv[i] - 1];
+      }
+
+      return decrypted;
+    };
+
+    key_generation(); // call to key generation function
+
+    // int []plaintext= {1,0,1,0,0,1,0,1};
+    // const plaintext = [1, 0, 0, 1, 0, 1, 1, 1]; // extra example for checking purpose
+
+    console.log();
+    console.log("Your plain Text is :");
+    console.log(plaintext.join(" ")); // printing the plaintext
+
+    const ciphertext = encryption(plaintext);
+
+    console.log();
+    console.log("Your cipher Text is :"); // printing the cipher text
+    console.log(ciphertext.join(" "));
+
+    const decrypted = decryption(ciphertext);
+
+    console.log();
+    console.log("Your decrypted Text is :"); // printing the decrypted text
+    console.log(decrypted.join(" "));
+    return (result = [ciphertext, decrypted]);
+  }
+}
+{
+  document.getElementById("des-encrypt").onclick = function () {
+    let myInt_k = document.getElementById("des-key").value;
+    let myFunc_k = (num_k) => Number(num_k);
+    let intArr_k = Array.from(String(myInt_k), myFunc_k);
+
+    let myInt_p = document.getElementById("des-text").value;
+    let myFunc_p = (num_p) => Number(num_p);
+    let intArr_p = Array.from(String(myInt_p), myFunc_p);
+
+    let result = des(intArr_k, intArr_p);
+    let digits = result[0];
+
+    let _result = digits.join("");
+
+    console.log(_result);
+    document.getElementById("des-result").value = _result;
+  };
+  document.getElementById("des-decrypt").onclick = function () {
+    let myInt_k = document.getElementById("des-key").value;
+    let myFunc_k = (num_k) => Number(num_k);
+    let intArr_k = Array.from(String(myInt_k), myFunc_k);
+
+    let myInt_p = document.getElementById("des-text").value;
+    let myFunc_p = (num_p) => Number(num_p);
+    let intArr_p = Array.from(String(myInt_p), myFunc_p);
+
+    let result = des(intArr_k, intArr_p);
+    let digits = result[1];
+
+    let _result = digits.join("");
+
+    console.log(_result);
+    document.getElementById("des-result").value = _result;
+  };
+}
